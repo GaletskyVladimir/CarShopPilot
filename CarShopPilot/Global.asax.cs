46a78ea266256a8a50364fffda0f3524c15485cc
Vladimir Galetsky <Uladzimir.Haletski@itechart-group.com>
2020-10-08 21:34:05 +0300

    Initial configuration

--- CarShopPilot/Global.asax.cs
+++ CarShopPilot/Global.asax.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,11 +16,18 @@ namespace CarShopPilot
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError();
+            //todo log ex
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            Response.ContentType = "application/json";
+            Response.ContentEncoding = Encoding.UTF8;
+            Response.Write(JsonConvert.SerializeObject(new { Message = "The request was invalid for the server" }));
+            Response.End();
         }
     }
 }
